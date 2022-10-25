5904dc892b425548dc7d47b3f0d9ab7a93bbccf5
Thomas LE CLERE <tleclere@jahia.com>
2022-10-25 18:55:39 +0200

    Groups SAML Connector Result Processor

---
+++ src/main/java/org/jahiacommunity/saml/groups/GroupProcessor.java
@@ -0,0 +1,85 @@
+package org.jahiacommunity.saml.groups;
+
+import org.jahia.api.usermanager.JahiaUserManagerService;
+import org.jahia.modules.jahiaauth.service.ConnectorConfig;
+import org.jahia.modules.jahiaauth.service.ConnectorResultProcessor;
+import org.jahia.modules.jahiaauth.service.JahiaAuthConstants;
+import org.jahia.services.content.decorator.JCRGroupNode;
+import org.jahia.services.content.decorator.JCRUserNode;
+import org.jahia.services.usermanager.JahiaGroupManagerService;
+import org.jahia.taglibs.user.User;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.RepositoryException;
+import java.util.List;
+import java.util.Map;
+
+@Component(service = ConnectorResultProcessor.class, immediate = true)
+public class GroupProcessor implements ConnectorResultProcessor {
+    private static final Logger logger = LoggerFactory.getLogger(GroupProcessor.class);
+
+    private JahiaUserManagerService jahiaUserManagerService;
+    private JahiaGroupManagerService jahiaGroupManagerService;
+
+    @Reference
+    private void setJahiaUserManagerService(JahiaUserManagerService jahiaUserManagerService) {
+        this.jahiaUserManagerService = jahiaUserManagerService;
+    }
+
+    @Reference
+    private void setJahiaGroupManagerService(JahiaGroupManagerService jahiaGroupManagerService) {
+        this.jahiaGroupManagerService = jahiaGroupManagerService;
+    }
+
+    @Override
+    public void execute(ConnectorConfig connectorConfig, Map<String, Object> results) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("SAML properties: {}", results);
+        }
+        try {
+            if (results.containsKey(JahiaAuthConstants.SSO_LOGIN)) {
+                String userId = (String) results.get(JahiaAuthConstants.SSO_LOGIN);
+                JCRUserNode userNode = jahiaUserManagerService.lookupUser(userId, connectorConfig.getSiteKey());
+                if (userNode != null) {
+                    removeUserMembership(userNode);
+                    if (results.containsKey("groups")) {
+                        manageUserGroups(userNode, connectorConfig.getSiteKey(), (List<String>) results.get("groups"));
+                    }
+                    userNode.saveSession();
+                }
+            }
+        } catch (RepositoryException e) {
+            logger.error("", e);
+        }
+    }
+
+    private void removeUserMembership(JCRUserNode userNode) {
+        User.getUserMembership(userNode).forEach((key, group) -> {
+            try {
+                group.removeMember(userNode);
+                group.saveSession();
+            } catch (RepositoryException e) {
+                logger.error("", e);
+            }
+        });
+    }
+
+    private void manageUserGroups(JCRUserNode userNode, String siteKey, List<String> groups) {
+        if (groups != null) {
+            groups.forEach(groupName -> {
+                try {
+                    JCRGroupNode group = jahiaGroupManagerService.lookupGroup(siteKey, groupName);
+                    if (group != null) {
+                        group.addMember(userNode);
+                        group.saveSession();
+                    }
+                } catch (RepositoryException e) {
+                    logger.error("", e);
+                }
+            });
+        }
+    }
+}
