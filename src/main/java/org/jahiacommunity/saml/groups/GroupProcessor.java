5836f157271d143739822994674ee9cbc5fc866c
Thomas LE CLERE <tleclere@jahia.com>
2022-10-25 18:56:41 +0200

    Configure groups attribute assignement

--- src/main/java/org/jahiacommunity/saml/groups/GroupProcessor.java
+++ src/main/java/org/jahiacommunity/saml/groups/GroupProcessor.java
@@ -1,5 +1,6 @@
 package org.jahiacommunity.saml.groups;
 
+import org.apache.commons.lang.StringUtils;
 import org.jahia.api.usermanager.JahiaUserManagerService;
 import org.jahia.modules.jahiaauth.service.ConnectorConfig;
 import org.jahia.modules.jahiaauth.service.ConnectorResultProcessor;
@@ -8,6 +9,7 @@ import org.jahia.services.content.decorator.JCRGroupNode;
 import org.jahia.services.content.decorator.JCRUserNode;
 import org.jahia.services.usermanager.JahiaGroupManagerService;
 import org.jahia.taglibs.user.User;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
@@ -23,6 +25,11 @@ public class GroupProcessor implements ConnectorResultProcessor {
 
     private JahiaUserManagerService jahiaUserManagerService;
     private JahiaGroupManagerService jahiaGroupManagerService;
+    private String groupsAttributeAssignment;
+
+    public GroupProcessor() {
+        groupsAttributeAssignment = "groups";
+    }
 
     @Reference
     private void setJahiaUserManagerService(JahiaUserManagerService jahiaUserManagerService) {
@@ -32,6 +39,13 @@ public class GroupProcessor implements ConnectorResultProcessor {
     @Reference
     private void setJahiaGroupManagerService(JahiaGroupManagerService jahiaGroupManagerService) {
         this.jahiaGroupManagerService = jahiaGroupManagerService;
+    }
+
+    @Activate
+    private void onActivate(Map<String, ?> configuration) {
+        if (configuration.containsKey("groups-attribute-assignement")) {
+            groupsAttributeAssignment = StringUtils.defaultIfBlank((String) configuration.get("groups-attribute-assignement"), "groups");
+        }
     }
 
     @Override
@@ -45,8 +59,8 @@ public class GroupProcessor implements ConnectorResultProcessor {
                 JCRUserNode userNode = jahiaUserManagerService.lookupUser(userId, connectorConfig.getSiteKey());
                 if (userNode != null) {
                     removeUserMembership(userNode);
-                    if (results.containsKey("groups")) {
-                        manageUserGroups(userNode, connectorConfig.getSiteKey(), (List<String>) results.get("groups"));
+                    if (results.containsKey(groupsAttributeAssignment)) {
+                        manageUserGroups(userNode, connectorConfig.getSiteKey(), (List<String>) results.get(groupsAttributeAssignment));
                     }
                     userNode.saveSession();
                 }
